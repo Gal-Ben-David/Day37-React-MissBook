9aeba7211d1f9fcad8bc1eab01d944eac63e4452
Gal Ben David <gal.benda3@gmail.com>
2024-10-09 20:25:07 +0300

    Display the provided demo-data

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -6,8 +6,8 @@ export function BookPreview({ book }) {
     return (
         <article className="book-preview">
             <h2>{book.title}</h2>
-            <p>{book.desc}</p>
-            <img src={book.imgSrc} />
+            <p>{book.description}</p>
+            <img src={book.thumbnail} />
             <p>{`Price: ${book.listPrice.amount}, ${book.listPrice.currencyCode}`}</p>
             <BookDetails />
         </article>
